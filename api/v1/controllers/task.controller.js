03d563eaf503d9f71223da6ef0246f0998474c1f
Pduc96421 <pduc96421@gmail.com>
2025-03-13 04:18:35 +0700

    Sắp xếp theo tiêu chíchí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -5,11 +5,19 @@ module.exports.index = async (req, res) => {
     const find = {
         deleted: false,
     }
+
+    //sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey] = req.query.sortValue;
+    }
+
     if (req.query.status) {
         find.status = req.query.status;
     }
 
-    const tasks = await Task.find(find);
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks);
 };
